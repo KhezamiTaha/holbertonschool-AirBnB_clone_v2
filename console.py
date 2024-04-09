52e66d98fbb1cf4da82857a17ef4a87258228cb8
KhezamiTaha <khezamitaha10@gmail.com>
2024-04-09 07:12:34 +0100

    Updating Create function in console to create an instance with attributes

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,15 +113,42 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+    def do_create(self, arg):
         """ Create an object of any class"""
-        if not args:
+        args = arg.split()
+        if not arg:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[args[0]]()
+
+        for parameter in args:
+            if parameter == args[0]:
+                continue
+            try:
+                key, value = parameter.split('=')
+            except ValueError:
+                continue
+            if value.startswith('\"') and value.endswith('\"'):
+                new_word = ""
+                for letter in value[1:len(value) - 1]:
+                    if letter == '\"':
+                        new_word += '\\'
+                    elif letter == '_':
+                        new_word += ' '
+                        continue
+                    new_word += letter
+                value = new_word
+            elif "." in value:
+                value = float(value)
+            else:
+                value = int(value)
+            try:
+                setattr(new_instance, key, value) # how to convert a string to a number 
+            except AttributeError:
+                continue
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +299,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +307,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
