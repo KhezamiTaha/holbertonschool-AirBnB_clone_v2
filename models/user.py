80fc26a3dc43491f7765f4d80eacc6418b752eb5
KhezamiTaha <khezamitaha10@gmail.com>
2024-04-15 17:11:03 +0100

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -16,12 +16,4 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+    
