26e5182720f2abbf07310d9ae2151b36087082d3
KhezamiTaha <khezamitaha10@gmail.com>
2024-04-22 08:49:49 +0100

    running first app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,5 +6,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello HBNB!</p>"
+def index():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
